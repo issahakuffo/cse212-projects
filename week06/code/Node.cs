a2cf2686c0d0aa3538c90ad3e6aca684f706ee02
Issah Akuffo <issahkuffo300@gmail.com>
2024-12-16 13:45:52 +0000

    week 6

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -24,22 +24,65 @@ public class Node
         else
         {
             // Insert to the right
-            if (Right is null)
-                Right = new Node(value);
-            else
-                Right.Insert(value);
+            if (value > Data)
+            {
+                if (Right is null)
+                    Right = new Node(value);
+                else
+                    Right.Insert(value);
+            }
         }
     }
 
     public bool Contains(int value)
     {
         // TODO Start Problem 2
+
+        if (value == Data)
+        {
+            return true;
+            // Insert to the left
+        }
+        else
+        {
+            if (value > Data)
+            {
+                if (Right is null)
+                    return false;
+                else
+                {
+                    Data = Right.Data; // Replace this line with the correct return statement(s)
+                    Contains(value);
+                }
+            }
+            else
+            {
+                if (Left is null)
+                    return false;
+                else
+                {
+                    Data = Left.Data; // Replace this line with the correct return statement(s)
+                    Contains(value);
+                }
+            }
+
+        }
         return false;
     }
 
     public int GetHeight()
     {
-        // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
+        // Base Case: If the node is null (this is handled implicitly via recursion), height is 0
+        if (this == null)
+        {
+            return 0;
+        }
+
+        // Recursive Case: Compute the height of left and right subtrees
+        int leftHeight = Left?.GetHeight() ?? 0; // Use null-coalescing to handle null cases
+        int rightHeight = Right?.GetHeight() ?? 0;
+
+        // Return the height of the current node
+        return 1 + Math.Max(leftHeight, rightHeight);
     }
 }
